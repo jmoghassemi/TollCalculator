51cd90f9c0452c3d733a25cac784bf07a9df85ac
root <root@DESKTOP-5N93TSR.localdomain>
2021-12-17 14:27:04 +0100

    added some comments

---
+++ Car.java
@@ -0,0 +1,9 @@
+package main;
+
+// The Car class which implements the getFee() method from the Vehicle Interface, and provides a specific definition for that method.
+
+public class Car implements Vehicle {
+    public int getFee() {
+        return 11;
+    }
+}
