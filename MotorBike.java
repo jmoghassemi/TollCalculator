03ac06df8b3b91af7253a47c25e5c8404d4c66f4
root <root@DESKTOP-5N93TSR.localdomain>
2021-12-20 13:22:59 +0100

    add/remove some comments

---
+++ MotorBike.java
@@ -0,0 +1,8 @@
+package main;
+
+// The MotorBike class which implements the getFee() method from the Vehicle Interface, and provides a specific definition for that method.
+public class MotorBike implements Vehicle {
+    public int getFee() {
+        return 8;
+    }
+}
