e6a153d783b4e02ad977a9f5a98601cb27da8626
root <root@DESKTOP-5N93TSR.localdomain>
2021-12-20 13:33:28 +0100

    add/remove some branches

---
+++ Vehicle.java
@@ -0,0 +1,6 @@
+package main;
+
+//The Vehicle Interface declaring the getFee() method, which is later uniquely defined by the Car and MotorBike classes.
+public interface Vehicle {
+    int getFee();
+}
